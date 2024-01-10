c67e39f0c8b9c1addb7302aac520b06e7d08490c
Florian Pescot-Jourdan <florianpescot4@gmail.com>
2024-01-10 15:15:32 +0100

    Ajout modification + alertes

--- script/api.js
+++ script/api.js
@@ -20,13 +20,13 @@ db.connect((err) => {
   console.log("Connecté à la base de données");
 });
 
-// Autoriser les requêtes en provenance de toutes les origines
+// Autoriser les requêtes en provenance de toutes les origines (à adapter en fonction de vos besoins de sécurité)
 app.use(cors({ origin: "*" }));
 
 // Middleware pour parser le corps de la requête en JSON
 app.use(express.json());
 
-// Créer une route pour récupérer les données
+// Créer une route pour récupérer les données (exemple)
 app.get("/donnees", (req, res) => {
   const query = "SELECT * FROM JOURNAL_API";
   db.query(query, (err, results) => {
@@ -34,13 +34,13 @@ app.get("/donnees", (req, res) => {
     res.send(results);
   });
 });
-
-app.put("/updateSensorName/:NUM_CAP", (req, res) => {
-  const { NUM_CAP } = req.params;
+app.put("/updateSensorName/:numCap", (req, res) => {
+  const { numCap } = req.params;
   const { newSensorName } = req.body;
 
+  // Mettez à jour le nom du capteur dans la base de données ici
   const query = "UPDATE JOURNAL_API SET NOM = ? WHERE NUM_CAP = ?";
-  db.query(query, [newSensorName, NUM_CAP], (err, results) => {
+  db.query(query, [newSensorName, numCap], (err, results) => {
     if (err) {
       res
         .status(500)
@@ -48,6 +48,28 @@ app.put("/updateSensorName/:NUM_CAP", (req, res) => {
     } else {
       res.status(200).json({ success: true });
     }
+  });
+});
+// Route pour créer une alerte
+app.post("/createAlert", (req, res) => {
+  const { numCap, temp, email } = req.body;
+
+  // Insérer les données dans la table "ALERTES" (à adapter selon votre structure)
+  const query = "INSERT INTO ALERTES (NUM_CAP, TEMP, EMAIL) VALUES (?, ?, ?)";
+  db.query(query, [numCap, temp, email], (err, results) => {
+    if (err) {
+      res.status(500).json({ error: "Erreur lors de la création de l'alerte" });
+    } else {
+      res.status(200).json({ success: true });
+    }
+  });
+});
+
+app.get("/alertes-actives", (req, res) => {
+  const query = "SELECT * FROM ALERTES"; // Supposons que vous avez une colonne "ACTIVE" pour indiquer si l'alerte est active
+  db.query(query, (err, results) => {
+    if (err) throw err;
+    res.send(results);
   });
 });
 
